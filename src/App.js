1b40599861c31b93d742992b7cd30dc0937310be
Christopher Swinton <chris@theswintons.co.uk>
2020-12-02 12:39:22 +0000

    added ucas logo and some styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageContainer>
+      <Header>
+        <Logo src={logo} alt="logo" />
+      </Header>
+    </PageContainer>
   );
 }
 
 export default App;
+
+const PageContainer = styled.div``
+
+const Header = styled.header``
+
+const Logo = styled.img`
+  height: 45px;
+`
