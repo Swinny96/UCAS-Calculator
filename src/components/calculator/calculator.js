a43fab09e2a6d9a59f9f95b6cd3720e88211c60b
Christopher Swinton <chris@theswintons.co.uk>
2020-12-02 15:12:17 +0000

    added points section

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -5,39 +5,48 @@ import styled from 'styled-components';
 class Calculator extends Component {
 	render() {
 		return (
-        <List>
-            <h3>Qualification</h3>
-            <CoursesSelector>
-                <Courses>
-                    <Course>Please select a qualification..</Course>
-                {
-					data.Experiences.map((experience, i) => {
-						return (
-								<Course key={i}>{experience.companyName}</Course>
-						);
-					})
-				}
-                </Courses>
-            </CoursesSelector>
-            <h3>Grade</h3>
-            <GradeSelector>
-                <Grades>
-                    <Grade>Please select a grade..</Grade>
-                {
-					data.Experiences.map((experience, i) => {
-						return (
-								<Grade key={i}>{experience.url}</Grade>
-						);
-					})
-				}
-                </Grades>
-            </GradeSelector>
-        </List>
+        <Container>
+            <Description>Working out your UCAS points can be a pain - especially when you have different types of qualifications. Luckily, our calculator is here to help. Just add your qualifications and let our UCAS Calculator do the maths!</Description>
+            <List>
+                <h3>Qualification</h3>
+                <CoursesSelector>
+                    <Courses>
+                        <Course>Please select a qualification..</Course>
+                        {
+                            data.Experiences.map((experience, i) => {
+                                return (
+                                        <Course key={i}>{experience.companyName}</Course>
+                                );
+                            })
+                        }
+                    </Courses>
+                </CoursesSelector>
+                <h3>Grade</h3>
+                <GradeSelector>
+                    <Grades>
+                        <Grade>Please select a grade..</Grade>
+                        {
+                            data.Experiences.map((experience, i) => {
+                                return (
+                                        <Grade key={i} value="12">{experience.url}</Grade>
+                                );
+                            })
+                        }
+                    </Grades>
+                </GradeSelector>
+                <Points>
+                    <PointsTitle>Your Points</PointsTitle>
+                    <PointsTotal>---</PointsTotal>
+                    <PointsText>Points</PointsText>
+                </Points>
+            </List>
+        </Container>
         );
     }
 } 
 export default Calculator;
-
+const Container = styled.div``
+const Description = styled.p``
 const List = styled.div``
 const CoursesSelector = styled.div``
 const Courses = styled.select``
@@ -45,3 +54,7 @@ const Course = styled.option``
 const GradeSelector = styled.div``
 const Grades = styled.select``
 const Grade = styled.option``
+const Points = styled.div``
+const PointsTitle = styled.h3``
+const PointsTotal = styled.span``
+const PointsText = styled.span``
