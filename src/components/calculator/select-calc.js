335b13ceccba2b10ffefa8959c07cf9c938576df
Christopher Swinton <33087553+Swinny96@users.noreply.github.com>
2020-12-17 14:36:22 +0000

    css added to Select Calc

--- src/components/calculator/select-calc.js
+++ src/components/calculator/select-calc.js
@@ -29,6 +29,7 @@ class SelectCalc extends Component {
           <Option value="20">C</Option>
           <Option value="10">D</Option>
         </Select>
+        <Arrow class="selector-arrow" aria-hidden="true" />
         <Points>
           Your Points: <PointsTotal>{this.state.value}</PointsTotal>
         </Points>
@@ -40,9 +41,52 @@ export default SelectCalc;
 
 const SelectForm = styled.form`
   margin: 30px;
+
+
+  @media (max-width: 600px) {
+    margin: 30px 0px;
+  }
 `;
 const Select = styled.select`
-  margin-right: 20px;
+  appearance: none;
+  background: linear-gradient(
+    to bottom,
+    rgba(255, 255, 255, 0) 0%,
+    rgba(204, 204, 204, 0.33) 100%
+  );
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  color: #111b40;
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 26px;
+  padding: 8px;
+  position: relative;
+  text-decoration: none;
+  width: 50%;
+  z-index: 2;
+
+  ::-ms-value {
+    background-color: transparent;
+    color: #111b40;
+  }
+
+  @media (max-width: 600px) {
+    width: 100%;
+    margin-bottom: 8px;
+  }
+`;
+const Arrow = styled.span`
+  border-left: 8px solid transparent;
+  border-right: 8px solid transparent;
+  border-top: 8px solid #333;
+  height: 0;
+  position: relative;
+  top: 6px;
+  right: 14px;
+  width: 0;
+  z-index: 1;
+  zoom: 1;
 `;
 const Option = styled.option``;
 const Points = styled.div`
